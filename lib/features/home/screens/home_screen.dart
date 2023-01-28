06ae7304e5baba1342748e0172f6d4ffe4f7428a
Sadid <sadid.jones@gmail.com>
2023-01-29 00:28:13 +0600

    @1/29/2023 @4:19

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:amazon/constants/global_verables.dart';
 import 'package:amazon/features/auth/services/auth_service.dart';
+import 'package:amazon/features/home/widgets/address_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -15,12 +17,81 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   Widget build(BuildContext context) {
     final user = ref.watch(userProvider)!;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(user.name),
-      ),
-      body: Center(
-        child: Text(user.email),
-      ),
-    );
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: AppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                      height: 42,
+                      margin: const EdgeInsets.only(
+                        left: 15,
+                      ),
+                      child: Material(
+                        borderRadius: BorderRadius.circular(7),
+                        elevation: 1,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              prefixIcon: InkWell(
+                                onTap: () {},
+                                child: const Padding(
+                                  padding: EdgeInsets.only(
+                                    left: 6,
+                                  ),
+                                  child: Icon(
+                                    Icons.search,
+                                    color: Colors.black,
+                                    size: 23,
+                                  ),
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              contentPadding: const EdgeInsets.only(top: 10),
+                              border: const OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(7),
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                              enabledBorder: const OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(7),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Colors.black38,
+                                  width: 1,
+                                ),
+                              ),
+                              hintText: "Search Amazon.in",
+                              helperStyle: const TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w500,
+                              )),
+                        ),
+                      )),
+                ),
+                Container(
+                  color: Colors.transparent,
+                  height: 42,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  child: const Icon(Icons.mic),
+                )
+              ],
+            ),
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                gradient: GlobalVariables.appBarGradient,
+              ),
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            AddressBox(),
+          ],
+        ));
   }
 }
